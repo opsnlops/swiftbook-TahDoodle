897d8e4bed760cd9f5fd9ed5c7cfc4843b85d70f
April White <april@opsnlops.io>
2022-12-03 21:04:46 -0800

    Initial Commit

---
+++ TahDoodle/TahDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahDoodleApp.swift
+//  TahDoodle
+//
+//  Created by April White on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TahDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
